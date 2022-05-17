49313a5ce4223185c84cae2049e0d8e9a0d4cda7
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-05-17 18:52:44 +0700

    submit record

--- servers/controllers/userController.js
+++ servers/controllers/userController.js
@@ -37,3 +37,65 @@ exports.view = (req, res) => {
         })
 	});
 };
+
+// &---- Find by search----
+exports.find = (req,res) => {
+    pool.getConnection((err, connection) => {
+			if (err) {
+				throw err;
+			}
+			console.log(`Connected as id ${connection.threadId}`);
+
+            let searchTerm = req.body.search
+			// &---- User Connection ----
+			connection.query(
+				'SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ?',
+				['%' + searchTerm + '%', '%' + searchTerm + '%'],
+				(err, rows) => {
+					// % saat berhasil
+					connection.release();
+
+					if (!err) {
+						res.render('home', {
+							rows,
+						});
+					} else {
+						console.log(`Error: ${err} `);
+					}
+					console.log(`Data dari user : \n ${rows}`);
+				}
+			);
+		});
+}
+
+// &---- Form ----
+exports.form = (req,res) => {
+	res.render('add-user')
+}
+
+// &---- Create ----
+exports.create = (req,res) => {
+	const{first_name, last_name,email,phone,comment} = req.body
+	pool.getConnection((err, connection) => {
+		if (err) {
+			throw err;
+		}
+		console.log(`Connected as id ${connection.threadId}`);
+
+		// &---- User Connection ----
+		connection.query(
+			'INSERT INTO user SET first_name = ?, last_name = ?, email = ?, phone = ?, comment = ?',[first_name, last_name,email,phone,comment],
+			(err, rows) => {
+				// % saat berhasil
+				connection.release();
+
+				if (!err) {
+					res.render('add-user',{alert:'User berhasil ditambah'});
+				} else {
+					console.log(`Error: ${err} `);
+				}
+				console.log(`Data dari user : \n ${rows}`);
+			}
+		);
+	});
+}
